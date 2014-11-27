5df47433ef77850f41f90cd6e1a9d3f2097c970b
vbloke <vbloke@gmail.com>
2014-11-27 12:25:10 +0000

    404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+	<div class="row" id="">
+		<div class="twelve columns">
+			<h3>404 Page Not Found</h3>
+			<p>The page you were looking for has either been deleted or moved. Please use the menu above to go to another section of the site</p>	
+		</div>
+	</div>
+
+<?php get_footer(); ?>	
